c3fcba64f49c4d82d0e58c97964e2ae591338417
wslxy000 <1825189419@qq.com>
2020-08-28 15:08:41 +0800

    登录完成

---
+++ js/axios.defaults.js
@@ -0,0 +1,31 @@
+axios.defaults.baseURL = "http://localhost:8888";
+
+axios.defaults.headers['Content-Type'] = 'application/x-www-form-urlencoded';
+axios.defaults.transformRequest = function(data){
+    if(!data) return data;
+    let result = ``;
+    for(let attr in data){
+        if(!data.hasOwnProperty(attr)) break;
+        result += `&${attr}=${data[attr]}`;
+    }
+    return result.substring(1);
+};
+//请求拦截器
+axios.interceptors.request.use(config => {
+    return config
+})
+axios.interceptors.response.use(response=> {
+    return response.data;
+}),reason=> {
+    // console.dir(reason)
+    if(reason.response){
+        switch (String(reason.response.status)){
+            case "404":
+                alert("地址不存在")
+                break;
+            default:
+                break;
+        }
+    }
+    return Promise.reject(reason)
+}
---
+++ js/login.js
@@ -0,0 +1,30 @@
+$(".submit").click(async function(){
+    let account = $(".userName").val().trim();
+    let password = $(".userPass").val().trim();
+    if(account == "" || password == ""){
+        alert("账号或密码不能为空！")
+        return;
+    }
+    password = md5(password);
+    // axios.post("/user/login",{
+    //     account,
+    //     password
+    // }).then(res=>{
+    //     if(parseInt(res.code) === 0){
+    //         alert("登录成功")
+    //         window.location.href="index.html"
+    //         return;
+    //     }
+    //     alert("用户名或密码错误")
+    // }).catch(err=>{
+    //     console.log(err)
+    // }) 
+    let res = await axios.post("/user/login",{account,password});
+    // console.log(res)
+    if(parseInt(res.code) === 0){
+        alert("登录成功")
+        window.location.href="index.html"
+        return;
+    }
+    alert("用户名或密码错误")
+})
